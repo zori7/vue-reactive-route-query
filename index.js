c0cdfb71a4fe3450295393e4a57487e77e3ba2aa
Zori Yeghikyan <zori.egikyan@gmail.com>
2019-12-18 16:19:51 +0400

    Main functionality

---
+++ index.js
@@ -0,0 +1,46 @@
+import hasIn from 'lodash.hasin'
+
+export default function createWatchQuery (propName, setter = null) {
+    return {
+        created () {
+            if (hasIn(this._computedWatchers, propName)) {
+                setter = setter || 'set' + propName.replace(/^\w/, letter => letter.toUpperCase())
+                if (!this.hasOwnProperty(setter))
+                    throw new Error('Please specify a valid setter')
+            }
+            else if (!hasIn(this, propName))
+                throw new Error('Property "' + propName + '" does not exist in data or computed properties')
+
+            if (this.$route.query[propName]) {
+                if (setter)
+                    this[setter](this.$route.query[propName])
+                else
+                    this[propName] = this.$route.query[propName]
+            }
+
+            this.$watch(propName, function (value) {
+                    let route = { query: { ...this.$route.query } }
+
+                    if (value === null) {
+                        if (this.$route.query.hasOwnProperty(propName)) {
+                            delete route.query[propName]
+                            this.$router.replace(route)
+                        }
+                        return
+                    }
+
+                    Object.defineProperty(route.query, propName, { value, enumerable: true })
+
+                    if (this.$route.query.hasOwnProperty(propName)) {
+                        if (this.$route.query[propName] !== value)
+                            this.$router.replace(route)
+                    } else
+                        this.$router.replace(route)
+                },
+                {
+                    immediate: true
+                }
+            )
+        }
+    }
+}
